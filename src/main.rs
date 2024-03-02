511a2b3924de0a83dc5bea95e433c9d1fda7d812
John Evans <lgastako@gmail.com>
2024-03-02 10:41:54 -0800

    Step 3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use clap::Parser;
+
+#[derive(Parser)]
 struct Cli {
     pattern: String,
     path: std::path::PathBuf,
@@ -6,13 +9,7 @@ struct Cli {
 fn main() {
     println!("Hello, world!");
 
-    let pattern = std::env::args().nth(1).expect("no pattern given");
-    let path    = std::env::args().nth(2).expect("no path given");
-
-    let args = Cli {
-        pattern: pattern,
-        path: std::path::PathBuf::from(path),
-    };
+    let args = Cli::parse();
 
     println!("pattern: {:?}, path: {:?}", args.pattern, args.path);
 }
