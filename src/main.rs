e6d763fbbc04d01a5fbc6c7b0448339d6d7002e2
John Evans <lgastako@gmail.com>
2024-03-02 10:37:02 -0800

    Step 2

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf,
+}
+
 fn main() {
     println!("Hello, world!");
 
     let pattern = std::env::args().nth(1).expect("no pattern given");
-    let path = std::env::args().nth(2).expect("no path given");
+    let path    = std::env::args().nth(2).expect("no path given");
 
-    println!("pattern: {:?}, path: {:?}", pattern, path)
+    let args = Cli {
+        pattern: pattern,
+        path: std::path::PathBuf::from(path),
+    };
+
+    println!("pattern: {:?}, path: {:?}", args.pattern, args.path);
 }
