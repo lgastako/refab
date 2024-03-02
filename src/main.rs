e28cfb27e6d42eb63e4ab48bf3e15416b950f69f
John Evans <lgastako@gmail.com>
2024-03-02 10:35:06 -0800

    Step 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
     println!("Hello, world!");
+
+    let pattern = std::env::args().nth(1).expect("no pattern given");
+    let path = std::env::args().nth(2).expect("no path given");
+
+    println!("pattern: {:?}, path: {:?}", pattern, path)
 }
