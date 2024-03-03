0bb9cef9d631a2259824331bf28d602393986476
John Evans <lgastako@gmail.com>
2024-03-02 22:57:16 -0800

    Add support for multiple pattern paths (hopefully).

--- src/fabric.rs
+++ src/fabric.rs
@@ -1,8 +1,24 @@
+use std::path::Path;
+
 pub fn load(command: String) -> Result<String, String> {
-    let var = "REFAB_FABRIC_PATTERNS_PATH".to_string();
+    let var = "REFAB_FABRIC_PATTERNS_PATHS".to_string();
     let error_msg = format!("{} environment variable", var);
-    let path = std::env::var(var).expect(&error_msg);
-    let full_path = format!("{}/{}/system.md", path, command);
+    let paths = std::env::var(var).expect(&error_msg);
+
+    let mut full_path = String::new();
+    for path in paths.split(";") {
+        let check_path_str: String = format!("{}/{}/system.md", path, command);
+        let check_path = Path::new(&check_path_str);
+        if std::path::Path::exists(&check_path) {
+            full_path.push_str(&check_path_str);
+            break
+        }
+    }
+
+    if full_path.is_empty() {
+        let msg = format!("Could not find a file for command '{}'. Checked paths: {}", command, paths);
+        return Err(msg.to_string());
+    }
 
     match std::fs::read_to_string(&full_path) {
         Ok(content) => {
